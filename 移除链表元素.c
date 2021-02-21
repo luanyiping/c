af8a7a3281f84199662a5fa2e90be8202f6c5bfc
luanyiping <3349244951@qq.com>
2021-02-21 23:12:42 +0800

    https://leetcode-cn.com/problems/remove-linked-list-elements/

---
+++ 移除链表元素.c
@@ -0,0 +1,30 @@
+struct ListNode* removeElements(struct ListNode* head, int val)
+{
+    struct ListNode* cur = head;
+    struct ListNode* prev = NULL;
+    while (cur != NULL)
+    {
+        if (cur->val == val)
+        {
+            if (cur == head)
+            {
+                head = cur->next;
+                free(cur);
+                cur = head;
+            }
+            else
+            {
+                prev->next = cur->next;
+                free(cur);
+                cur = prev->next;
+            }
+        }
+        else
+        {
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return head;
+
+}

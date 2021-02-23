6588a992495a460976a2b6185975a990a5d4303c
luanyiping <75729730+luanyiping@users.noreply.github.com>
2021-02-23 21:08:45 +0800

    Update 移除链表元素.c

--- 移除链表元素.c
+++ 移除链表元素.c
@@ -28,3 +28,46 @@ struct ListNode* removeElements(struct ListNode* head, int val)
     return head;
 
 }
+
+
+typedef struct ListNode ListNode;
+ListNode* CreateListNode()
+{
+    ListNode* DummyNode = (ListNode*)malloc(sizeof(ListNode));
+    if (DummyNode == NULL)
+    {
+        exit(-1);
+    }
+    else
+    {
+        DummyNode->next = NULL;
+    }
+    return DummyNode;
+}
+struct ListNode* removeElements(struct ListNode* head, int val)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    ListNode* DummyNode = CreateListNode();
+    DummyNode->next = head;
+    head = DummyNode;
+    ListNode* prev = head, * cur = head->next;
+    while (cur != NULL)
+    {
+        if (cur->val == val)
+        {
+            prev->next = cur->next;
+            free(cur);
+            cur = prev->next;
+        }
+        else
+        {
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+    return head->next;
+
+}

0866f50acbfe75bbfe93fa94b306ac174fd478d7
luanyiping <75729730+luanyiping@users.noreply.github.com>
2021-02-27 09:56:56 +0800

    Update 判断链表是否带环.c

--- 判断链表是否带环.c
+++ 判断链表是否带环.c
@@ -23,5 +23,19 @@ bool hasCycle(struct ListNode* head)
         }
     }
     return false;
+    
+    
+     while (fast && fast->next)
+    {
+       
+            slow = slow->next;
+            fast =  fast->next->next;
+            if (fast == slow)
+          {
+            
+                return true;
+           }
+    }
+    return false;
 
 }
